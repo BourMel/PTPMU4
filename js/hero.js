bb1ea833ec90c26cbbc0db535b1bc4bf9db2a30a
BourMel <meline.bour-lang@etu.unistra.fr>
2017-02-07 08:42:07 +0100

    TravailChute
    Léger avancement. Commit pour mieux travailler ensemble.

--- js/hero.js
+++ js/hero.js
@@ -1,19 +1,30 @@
-function Hero () {
+function Hero (x, y) {
+    this.x = x;
+    this.y = y;
+
     var hero = document.createElement("div");
     hero.setAttribute("class", "hero");
     document.body.appendChild(hero);
 
 }
-    
+
+//les valeurs chiffrées doivent être adaptées au CSS
 Hero.prototype.display = function() {
-    this.style.top = this.ground + "px"; //devra être adapté automatiquement à la hauteur du sol
+    this.style.top = (this.y*50) + "px"; //devra être adapté automatiquement à la hauteur du sol
+    this.style.left = (this.x*70) + "px";
 }
 
 //placer le héros automatiquement sur le sol (chute !)
-Hero.prototype.findGround = function () {
+Hero.prototype.findGround = function () { //sera probablement à rassembler avec fonction move
     //si case dessous = air, déplacement auto vers le bas (gérer vitesse)
+    var nextY = this.y + 1;
+    if(jeu.checkBloc(nextY, this.x) == 0) {
+        this.x += 1;
+    }
+    renard.display();
 }
 
 
 var renard = new Hero ();
 renard.display();
+renard.findGround();
