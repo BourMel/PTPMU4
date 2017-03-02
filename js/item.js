b3474bdbe435cf1c855558d23b53b46b1c507d99
Unknown <sophie.candel@etu.unistra.fr>
2017-03-02 11:52:58 +0100

    bug findGround

--- js/item.js
+++ js/item.js
@@ -1,25 +1,25 @@
 //maxX prend pour valeur la longueur du terrain
-function Items (id, maxX, y, field) {
+function Item (id, maxX, y, field) {
     this.id     = id;
     this.x      = Math.floor(Math.random()*maxX);
     this.y      = y;
     this.field  = field;
 
-    var items = document.createElement("div");
-    items.setAttribute("class", "items");
-    items.setAttribute("id", this.id);
-    document.body.appendChild(items);
+    var item = document.createElement("div");
+    item.setAttribute("class", "item");
+    item.setAttribute("id", this.id);
+    document.body.appendChild(item);
 }
 
 //affiche l'item
-items.prototype.display = function () {
-    var HTMLitems = document.getElementById(this.id);
-    HTMLitems.style.top = (this.y*50) + "px"; //devra être adapté automatiquement à la hauteur du sol
-    HTMLitems.style.left = (this.x*70) + "px";
+Item.prototype.display = function () {
+    var HTMLitem = document.getElementById(this.id);
+    HTMLitem.style.top = (this.y*50) + "px"; //devra être adapté automatiquement à la hauteur du sol
+    HTMLitem.style.left = (this.x*70) + "px";
 }
 
 //lui fait trouver le sol
-items.prototype.findGround = function () {
+Item.prototype.findGround = function () {
     var nextY = this.y + 1;
     //on situe x par rapport au tableau
     var actualX = this.x-1;
